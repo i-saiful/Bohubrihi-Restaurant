1fc77b46ec8121f8b23e9acec731fcce3867ca9b
Saiful-Lab <saifulislam_@outlook.com>
2022-08-17 20:07:14 +0600

    page add component

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import Menu from './Menu'
+import Menu from './Menu';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
 
 function Body() {
   return (
