23881a827fa6b79f5dd7a55f50d1e918c1e3ba00
Saiful-Lab <saifulislam_@outlook.com>
2022-08-17 21:33:25 +0600

    navigation design

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,16 +1,49 @@
-import React from 'react'
-import {Navbar, NavbarBrand} from 'reactstrap';
+import React, { Component } from 'react'
+import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
-function Navigation() {
-  return (
-    <div>
-        <Navbar dark color='dark'>
-            <div className="container">
-                  <NavbarBrand href='/'>Bohubrihi Restaurant</NavbarBrand>
-            </div>
+class Navigation extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isNavOpen: false
+    }
+  }
+
+  navToggle = () => {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color='dark' expand='sm'>
+          <div className="container">
+            <NavbarToggler onClick={this.navToggle}/>
+            <NavbarBrand href='/'>Bohubrihi Restaurant</NavbarBrand>
+            <Collapse navbar isOpen={this.state.isNavOpen}>
+              <Nav className='mr-auto' navbar>
+                <NavItem>
+                  <Link to='/' className='nav-link active'>Home</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/menu' className='nav-link '>menu</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/about' className='nav-link '>About</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/contact' className='nav-link '>Contact</Link>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
         </Navbar>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default Navigation
